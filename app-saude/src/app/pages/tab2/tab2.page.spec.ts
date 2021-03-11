9001dc799526823a411c3d8a027129ed605d9d75
AmadoMaria <mariantoniamado@hotmail.com>
2021-03-10 21:48:15 -0300

    implementing Tab2 tests
    - the test "should call onSubmit method" is pending

--- app-saude/src/app/pages/tab2/tab2.page.spec.ts
+++ app-saude/src/app/pages/tab2/tab2.page.spec.ts
@@ -1,24 +1,189 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule, By } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
 
 import { Tab2Page } from './tab2.page';
 
-xdescribe('Tab2Page', () => {
+describe('Tab2Page', () => {
   let component: Tab2Page;
   let fixture: ComponentFixture<Tab2Page>;
+  let de: DebugElement;
+  let el: HTMLElement;
+  let httpTestingController: HttpTestingController;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [Tab2Page],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [Tab2Page],
+        imports: [
+          IonicModule.forRoot(),
+          HttpClientTestingModule,
+          FormsModule,
+          ReactiveFormsModule,
+          BrowserModule,
+        ],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab2Page);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(Tab2Page);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+
+      httpTestingController = TestBed.get(HttpTestingController);
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  xit('should call onSubmit method', () => {
+    //sometimes thsi test pass others no
+    fixture.detectChanges();
+    spyOn(component, 'onFormEvaluationSubmit');
+
+    el = fixture.debugElement.query(By.css('#submitButton')).nativeElement;
+    el.click();
+    expect(component.onFormEvaluationSubmit).toHaveBeenCalledTimes(0);
+    expect(component.formEvaluation.valid).toBeFalsy();
+  });
+
+  it('form should be valid', () => {
+    component.formEvaluation.controls['pSistolica'].setValue(145);
+    component.formEvaluation.controls['pDiastolica'].setValue(95);
+    component.formEvaluation.controls['pulso'].setValue(90);
+
+    expect(component.formEvaluation.valid).toBeTruthy();
+  });
+
+  describe('form should be invalid', () => {
+    it('if systolic pressure is invalid', () => {
+      component.formEvaluation.controls['pSistolica'].setValue('');
+      component.formEvaluation.controls['pDiastolica'].setValue(95);
+      component.formEvaluation.controls['pulso'].setValue(90);
+
+      expect(component.formEvaluation.valid).toBeFalsy();
+    });
+
+    it('if diastolic pressure is invalid', () => {
+      component.formEvaluation.controls['pSistolica'].setValue(145);
+      component.formEvaluation.controls['pDiastolica'].setValue('');
+      component.formEvaluation.controls['pulso'].setValue(90);
+
+      expect(component.formEvaluation.valid).toBeFalsy();
+    });
+
+    it('if pulse is invalid', () => {
+      component.formEvaluation.controls['pSistolica'].setValue(145);
+      component.formEvaluation.controls['pDiastolica'].setValue(95);
+      component.formEvaluation.controls['pulso'].setValue('');
+
+      expect(component.formEvaluation.valid).toBeFalsy();
+    });
+  });
+
+  describe('hypertensionClassification', () => {
+    it('should be normal if diastolic pressure is less than 85 and systolic pressure is less than 130', () => {
+      fixture.detectChanges();
+
+      component.formEvaluation.controls['pSistolica'].setValue(120);
+      component.formEvaluation.controls['pDiastolica'].setValue(80);
+
+      el = fixture.debugElement.query(By.css('#submitButton')).nativeElement;
+      el.click();
+
+      expect(component.evaluation.hipertensao).toBe('Normal')
+    });
+
+    it('should be prehypertension if diastolic pressure is betwenn 85 and 89 and systolic pressure is between 130 and 139', () => {
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('#submitButton')).nativeElement;
+
+      component.formEvaluation.controls['pSistolica'].setValue(139);
+      component.formEvaluation.controls['pDiastolica'].setValue(85);
+
+
+      el.click();
+
+      expect(component.evaluation.hipertensao).toBe('Normal limítrofe')
+
+      component.formEvaluation.controls['pSistolica'].setValue(130);
+      component.formEvaluation.controls['pDiastolica'].setValue(89);
+
+      el.click();
+
+      expect(component.evaluation.hipertensao).toBe('Normal limítrofe')
+
+    });
+
+    it('should be stage 1 hypertension if diastolic pressure is betwenn 90 and 99 and systolic pressure is between 140 and 159', () => {
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('#submitButton')).nativeElement;
+
+      component.formEvaluation.controls['pDiastolica'].setValue(90);
+      component.formEvaluation.controls['pSistolica'].setValue(140);
+
+      el.click();
+
+      expect(component.evaluation.hipertensao).toBe('Hipertensão leve (estágio 1)')
+
+      component.formEvaluation.controls['pDiastolica'].setValue(99);
+      component.formEvaluation.controls['pSistolica'].setValue(159);
+
+      el.click();
+
+      expect(component.evaluation.hipertensao).toBe('Hipertensão leve (estágio 1)')
+
+    });
+
+    it('should be stage 2 hypertension if diastolic pressure is betwenn 100 and 109 and systolic pressure is between 160 and 179', () => {
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('#submitButton')).nativeElement;
+
+      component.formEvaluation.controls['pDiastolica'].setValue(100);
+      component.formEvaluation.controls['pSistolica'].setValue(160);
+
+      el.click();
+
+      expect(component.evaluation.hipertensao).toBe('Hipertensão moderada (estágio 2)')
+
+      component.formEvaluation.controls['pDiastolica'].setValue(109);
+      component.formEvaluation.controls['pSistolica'].setValue(179);
+
+      el.click();
+
+      expect(component.evaluation.hipertensao).toBe('Hipertensão moderada (estágio 2)')
+
+    });
+
+    it('should be hypertensive crises if diastolic pressure is greater than 110 and systolic pressure is greater than 180', () => {
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('#submitButton')).nativeElement;
+
+      component.formEvaluation.controls['pDiastolica'].setValue(115);
+      component.formEvaluation.controls['pSistolica'].setValue(185);
+
+      el.click();
+
+      expect(component.evaluation.hipertensao).toBe('Hipertensão grave (estágio 3)')
+    });
+
+    it('should be isolated systolic hypertension if diastolic pressure is less than 90 and systolic pressure is greater than 140', () => {
+      fixture.detectChanges();
+      el = fixture.debugElement.query(By.css('#submitButton')).nativeElement;
+
+      component.formEvaluation.controls['pDiastolica'].setValue(80);
+      component.formEvaluation.controls['pSistolica'].setValue(150);
+
+      el.click();
+
+      expect(component.evaluation.hipertensao).toBe('Hipertensão sistólica isolada')
+    });
+
   });
 });
